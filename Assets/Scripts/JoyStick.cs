8f1cbed30cafc57b60cf2c59ab1653f594188b92
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-05-19 16:55:43 +0200

    Joystick OK

---
+++ Assets/Scripts/JoyStick.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStick : MonoBehaviour
+{
+
+    public Transform player;
+    public float speed = 5.0f;
+
+    private bool touchStart = false;
+
+    private Vector2 pointA;
+    private Vector2 pointB;
+
+
+    public Transform circle;
+    public Transform outerCircle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetMouseButtonDown(0)){
+
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            // No screen view da o default value
+            circle.transform.position = new Vector3(pointA.x, pointA.y, circle.position.z);;
+            outerCircle.transform.position = new Vector3(pointA.x, pointA.y, outerCircle.position.z);;
+            
+            circle.GetComponent<SpriteRenderer>().enabled = true;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        if(Input.GetMouseButton(0)){
+            touchStart = true;
+
+            pointB = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+
+            //pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            // No screen view da o default value
+        }else{
+
+            touchStart = false;
+            circle.GetComponent<SpriteRenderer>().enabled = false;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+
+        //moveCharacter(new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")));
+    }
+
+
+
+    private void FixedUpdate(){
+
+        if(touchStart){
+
+            Vector2 offset = pointB - pointA;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            moveCharacter(direction);
+            
+            circle.transform.position = new Vector3(pointA.x + direction.x, pointA.y + direction.y, circle.position.z);
+
+            //circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y);
+
+        }
+
+    }
+
+    void moveCharacter(Vector2 direction){
+
+        player.Translate(direction * speed * Time.deltaTime);// suave durante os frames
+        
+    }
+
+
+}
