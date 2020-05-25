a9cd81563e32fcff59c916e3c2f018e2d8cf2291
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-05-25 18:31:34 +0200

    Front_Login_OK

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void LoadMenu(){
+        
+        SceneManager.LoadScene("Menu");
+
+    }
+
+
+
+}
