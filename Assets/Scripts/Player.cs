6e339865f45365bf50c4098c3b4ca715928f69ba
Jose-Verdu-Diaz <pepv@hotmail.es>
2020-06-02 16:43:54 +0200

    Añadido sistema de pickup de objetos
    - Nueva carpeta SFX para efectos de sonido
    - Objeto "brocoli" añadido
    - Efecto de "flotación" para animar los objetos

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public AudioSource ItemPickup;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {     
+        if (other.tag == "Item")
+        {
+            other.gameObject.SetActive(false);
+            ItemPickup.Play();
+            Debug.Log("Objeto recogido");
+        }
+    }
+}
