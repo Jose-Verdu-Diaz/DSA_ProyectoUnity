5a9c74f1ef5f4152791234f85f25be7c9d8d0adf
Jose-Verdu-Diaz <pepv@hotmail.es>
2020-06-13 18:34:32 +0200

    Prefab Estornudo añadido

---
+++ Assets/Scripts/RotacionEstornudo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionEstornudo : MonoBehaviour
+{
+    public float frequency = 1f;
+    public float rotation = 0.15f;
+    public float destroyTime = 5;
+
+    public Sprite[] sprites;
+
+    Quaternion posOffset = new Quaternion();
+    Quaternion tempPos = new Quaternion();
+
+    void Start()
+    {
+        transform.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+    }
+
+    void Update()
+    {
+
+        Color tmp = transform.GetComponent<SpriteRenderer>().color;
+        tmp.a = Mathf.Sin(Time.fixedTime * Mathf.PI * frequency)*0.25f + 0.5f;
+        transform.GetComponent<SpriteRenderer>().color = tmp;
+
+        transform.Rotate(new Vector3(0f,0f, rotation));
+
+        Destroy(gameObject, destroyTime);
+    }
+}
