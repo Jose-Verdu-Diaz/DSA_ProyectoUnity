f79b50d89ae94b912a1f3e19de2a88213cdd2358
Jose-Verdu-Diaz <pepv@hotmail.es>
2020-06-14 16:18:59 +0200

    JoyStick completamente implementado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,20 @@ public class Player : MonoBehaviour
 {
     public AudioSource ItemPickup;
 
+    protected Joystick joystick;
+    Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rigidBody.velocity = new Vector3(joystick.Horizontal * 4f, joystick.Vertical * 4f, 0f);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {     
         if (other.tag == "Item")
