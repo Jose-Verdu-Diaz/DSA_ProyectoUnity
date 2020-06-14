f2cba33e6d2a6d2e29611a42e6102dcf578b248d
Jose-Verdu-Diaz <pepv@hotmail.es>
2020-06-14 15:24:43 +0200

    Spwaneo de estornudo añadido a los WanderingNPC

--- Assets/Scripts/NPC/Estornudo/SpawnEstornudo.cs
+++ Assets/Scripts/NPC/Estornudo/SpawnEstornudo.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class SpawnEstornudo : MonoBehaviour
 {
+    public GameObject estornudo;
+    public bool enfermo = true;
+    public int probabiliadEstornudo;
+    private int p;
+    private Vector3 offset;
+    private int pausaEstornudar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pausaEstornudar = 1000;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (pausaEstornudar==0)
+        {
+            p = Random.Range(0,10000);
+            if (p <= probabiliadEstornudo)
+            {
+                offset = transform.position;
+                Instantiate(estornudo, offset, Quaternion.identity);
+                offset.x = offset.x + 0.1f;
+                Instantiate(estornudo, offset, Quaternion.identity);
+                offset.x = offset.x - 0.2f;
+                Instantiate(estornudo, offset, Quaternion.identity);
+                offset.x = offset.x + 0.1f;
+                offset.y = offset.y + 0.1f;
+                Instantiate(estornudo, offset, Quaternion.identity);
+                offset.y = offset.y - 0.2f;
+                Instantiate(estornudo, offset, Quaternion.identity);
+
+                pausaEstornudar = 1000;
+            }
+        }
+        else
+        {
+            pausaEstornudar--;
+        }
         
     }
 }
